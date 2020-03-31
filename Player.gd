8a77030f2b21f63aa60d16fe61dab37beabb5bcf
yaqb1 <32592543+yaqb1@users.noreply.github.com>
2020-03-31 20:23:42 +0200

    Created project
    Added World (main) scene + Player scene with player movement

---
+++ Player.gd
@@ -0,0 +1,19 @@
+extends KinematicBody2D
+
+const FRICTION = 300
+const MAX_SPEED = 200
+var velocity = Vector2.ZERO
+
+func _physics_process(delta):
+	var input_vector = Vector2.ZERO
+	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
+	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
+	input_vector = input_vector.normalized()
+	
+	if input_vector != Vector2.ZERO:
+		velocity = input_vector * MAX_SPEED
+	else:
+		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
+	
+	move_and_collide(velocity * delta)
+		
