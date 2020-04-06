1615af44d7e19976e902a1975b0243194cda3e11
Edwin Żurawik <ezurawik@poczta.fm>
2020-04-06 23:14:23 +0200

    Added sign stats + fixed not being able to put out fire

--- Menu.gd
+++ Menu.gd
@@ -3,8 +3,8 @@ extends Control
 
 
 func _on_Button_pressed():
-	get_tree().change_scene("res://World.tscn")
+    get_tree().change_scene("res://World.tscn")
 
 
 func _on_Button2_pressed():
-	get_tree().quit()
+    get_tree().quit()
--- trap.gd
+++ trap.gd
@@ -4,34 +4,35 @@ var canInteract = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	$close.hide()
-	pass
+    $close.hide()
+    pass
 
 func destroyTrap():
-	$close.show()
-	$trap.hide()
-	$CollisionShape2D.set_deferred("disabled",true)
-	yield(get_tree().create_timer(1), "timeout")
-	$CollisionShape2D.set_deferred("disabled",false)
-	GlobalWorld.trap_count -= 1
-	queue_free()
-	
-	
+    $close.show()
+    $trap.hide()
+    $CollisionShape2D.set_deferred("disabled",true)
+    yield(get_tree().create_timer(1), "timeout")
+    $CollisionShape2D.set_deferred("disabled",false)
+    GlobalWorld.trap_count -= 1
+    GlobalWorld.destroyed_traps += 1
+    queue_free()
+    
+    
 func _on_trap_body_entered(body):
-	if body.is_in_group('Player'):
-		canInteract = true
+    if body.is_in_group('Player'):
+        canInteract = true
 
 # warning-ignore:unused_argument
 func _physics_process(delta):
-	interactWithTrap()
-	
+    interactWithTrap()
+    
 func interactWithTrap():
-	if canInteract and Input.is_action_pressed("ui_select"):
-		if GlobalWorld.tools["toolsBox"] :
-			destroyTrap()
-	
-	
+    if canInteract and Input.is_action_pressed("ui_select"):
+        if GlobalWorld.tools["toolsBox"] :
+            destroyTrap()
+    
+    
 func _on_trap_body_exited(body):
-	if body.is_in_group('Player'):
-		canInteract = false
+    if body.is_in_group('Player'):
+        canInteract = false
 
