357d917522fdf0497231f41e00153ce93546b3b0
yaqb1 <32592543+yaqb1@users.noreply.github.com>
2020-04-06 21:31:34 +0200

    Added Main Menu scene
    - menu scene
    - added walls in game

--- trap.gd
+++ trap.gd
@@ -6,32 +6,32 @@ signal removeTrap
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-    pass
+	pass
 
 func destroyTrap():
-    emit_signal("removeTrap")
-    GlobalWorld.destroyed_traps += 1  # Trap stats
-    $CollisionShape2D.set_deferred("disabled",true)
-    yield(get_tree().create_timer(0.5), "timeout")
-    $CollisionShape2D.set_deferred("disabled",false)
-    
-    
+	emit_signal("removeTrap")
+	GlobalWorld.destroyed_traps += 1  # Trap stats
+	$CollisionShape2D.set_deferred("disabled",true)
+	yield(get_tree().create_timer(0.5), "timeout")
+	$CollisionShape2D.set_deferred("disabled",false)
+	
+	
 func _on_trap_body_entered(body):
-    if body.is_in_group('Player'):
-        canInteract = true
+	if body.is_in_group('Player'):
+		canInteract = true
 
 # warning-ignore:unused_argument
 func _physics_process(delta):
-    interactWithTrap()
-    
+	interactWithTrap()
+	
 func interactWithTrap():
-    if canInteract and Input.is_action_pressed("ui_select"):
-        if GlobalWorld.tools["toolsBox"] :
-            print('trap destroy')
-            destroyTrap()
-    
-    
+	if canInteract and Input.is_action_pressed("ui_select"):
+		if GlobalWorld.tools["toolsBox"] :
+			print('trap destroy')
+			destroyTrap()
+	
+	
 func _on_trap_body_exited(body):
-    if body.is_in_group('Player'):
-        canInteract = false
+	if body.is_in_group('Player'):
+		canInteract = false
 
